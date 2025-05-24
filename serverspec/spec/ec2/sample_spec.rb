0a49952d8b5f6fdb78d6dcfac6b23325a4232341
EC2 Default User <ec2-user@ip-172-31-5-230.ap-northeast-1.compute.internal>
2025-05-24 13:18:08 +0000

    change Serverspec sample_spec.rb

--- serverspec/spec/ec2/sample_spec.rb
+++ serverspec/spec/ec2/sample_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 # ----------------------------
 # Ruby / Bundler / Rails / Node.js / Yarn
 # ----------------------------
-describe command('sudo -E bash -l -c "ruby -v"') do
+describe command('ruby -v') do
+  let(:disable_sudo) { true }
   its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 
