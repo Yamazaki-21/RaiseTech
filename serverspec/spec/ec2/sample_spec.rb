55d114160c78b816742530b2f304173e62114863
EC2 Default User <ec2-user@ip-172-31-5-230.ap-northeast-1.compute.internal>
2025-05-25 06:55:07 +0000

    change Serverspec sample_spec.rb

--- serverspec/spec/ec2/sample_spec.rb
+++ serverspec/spec/ec2/sample_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 # Ruby / Bundler / Rails / Node.js / Yarn バージョン確認
-describe command('/bin/bash -lc "ruby -v"') do
+shared_shell = [
+  'export PATH="$HOME/.rbenv/bin:$PATH"',
+  'eval "$(rbenv init -)"'
+].join(' && ')
+describe command(%Q(/bin/bash -lc "#{shared_shell} && ruby -v")) do
   let(:disable_sudo) { true }
-  its(:stdout) { should match /ruby 3\.2\.3/ }
+  its(:stdout) { should match %r{\Aruby 3\.2\.3} }
 end
-describe command('/bin/bash -lc "bundler -v"') do
+describe command(%Q(/bin/bash -lc "#{shared_shell} && bundler -v")) do
   let(:disable_sudo) { true }
-  its(:stdout) { should match /Bundler version 2\.3\.14/ }
+  its(:stdout) { should match %r{Bundler version 2\.3\.14} }
 end
-describe command('/bin/bash -lc "rails -v"') do
+describe command(%Q(/bin/bash -lc "#{shared_shell} && rails -v")) do
   let(:disable_sudo) { true }
-  its(:stdout) { should match /Rails 7\.1\.3\.2/ }
+  its(:stdout) { should match %r{\ARails 7\.1\.3\.2} }
 end
 describe command('node -v') do
   let(:disable_sudo) { true }
