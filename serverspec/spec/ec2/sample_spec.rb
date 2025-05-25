466bcceba3a48725177c46ff024286b8d85dbce9
EC2 Default User <ec2-user@ip-172-31-5-230.ap-northeast-1.compute.internal>
2025-05-25 06:16:57 +0000

    change Serverspec sample_spec.rb

--- serverspec/spec/ec2/sample_spec.rb
+++ serverspec/spec/ec2/sample_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 # Ruby / Bundler / Rails / Node.js / Yarn バージョン確認
 describe command('/bin/bash -lc "ruby -v"') do  # rbenv 環境で確認
+  let(:disable_sudo) { true }
   its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 describe command('/bin/bash -lc "bundler -v"') do
+  let(:disable_sudo) { true }
   its(:stdout) { should match /Bundler version 2\.3\.14/ }
 end
 describe command('/bin/bash -lc "rails -v"') do
+  let(:disable_sudo) { true }
   its(:stdout) { should match /Rails 7\.1\.3\.2/ }
 end
 describe command('node -v') do
